a65a10f8351bf3dcb34236e2dfd35fbeca615504
Yuanhui He <13507772589@163.com>
2021-01-02 14:50:18 +0800

    git init sp5v210-linux-3.0.8

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sat Jan 2 14:44:23 CST 2021"
+#define LINUX_COMPILE_BY "lithev"
+#define LINUX_COMPILE_HOST "X300"
+#define LINUX_COMPILER "gcc version 4.5.1 (ctng-1.8.1-FA) "
